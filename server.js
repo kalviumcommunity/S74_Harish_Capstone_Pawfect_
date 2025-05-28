ae5ae439139b44b9cd2ff673803e07e3d5547f97
Harish-Vinayagam <vinhari2212@gmail.com>
2025-05-28 16:44:04 +0530

    done

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('Pawfect backend is running!');
+});
+
+
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB connected'))
     .catch((err) => console.error(err));
